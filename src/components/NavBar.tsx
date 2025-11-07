99d999c99e7e99ac38c3bc1bac58f8e5b19d8300
Cabrel KEPSEU <cabrelk001@gmail.com>
2025-11-07 05:47:30 +0100

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from "next/image";
+import {navLinks} from "@/constants";
+
+const NavBar = () => {
+
+    return (
+        <header>
+            <nav>
+                <Image src="/logo.svg" width={24} height={24} alt="Apple logo" />
+                <ul>
+                    {navLinks.map(({label}) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="flex-center gap-3">
+                    <button>
+                        <Image src="/search.svg" alt={`Search`} />
+                    </button>
+                    <button>
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img alt="Cart" src="/cart.svg"/>
+                    </button>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
