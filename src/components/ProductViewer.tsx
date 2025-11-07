d78b24e33fb5f165b42126cab3d75f0b376e7a30
Cabrel KEPSEU <cabrelk001@gmail.com>
2025-11-07 13:52:07 +0100

    feat: Add lights Ambient in 3D Models

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 import {Canvas} from "@react-three/fiber"
 import {Box, OrbitControls} from "@react-three/drei"
 import MacbookModel14 from "@/components/models/Macbook-14"
+import StudioLights from './StudioLights';
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
@@ -47,7 +48,7 @@ const ProductViewer = () => {
 
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
                 {/* <Box position={[0, 0, 0]} scale={10 * scale} material-color={color}/> */}
-                <ambientLight intensity={1}/>
+                <StudioLights />
                 <MacbookModel14 position={[0, 0, 0]} scale={0.06}/>
 
                 <OrbitControls enableZoom={false}/>
---
+++ src/components/StudioLights.jsx
@@ -0,0 +1,48 @@
+"use client"
+import { Environment, Lightformer } from '@react-three/drei'
+import React from 'react'
+
+const StudioLights = () => {
+  return (
+    <group name="lights">
+      <Environment resolution={256}>
+        <group>
+          <Lightformer
+            intensity={10}
+            form="rect"
+            scale={10}
+            rotation-y={Math.PI / 2}
+            position={[10, 5, -5]}
+          />
+          <Lightformer
+            intensity={10}
+            form="rect"
+            scale={10}
+            rotation-y={Math.PI / 2}
+            position={[10, 0, 1]}
+          />
+        </group>
+      </Environment>
+      <spotlight
+        position={[-2, 10, 5]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
+      />
+      <spotlight
+        position={[0, -25, 10]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
+      />
+      <spotlight
+        position={[0, 15, 5]}
+        angle={0.15}
+        decay={0.1}
+        intensity={Math.PI * 0.2}
+      />
+    </group>
+  )
+}
+
+export default StudioLights
